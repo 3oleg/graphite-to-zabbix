13bdf41cb686c1bd37c434b676a9b6cd27281828
Zhang He <tgavriltg@gmail.com>
2016-06-20 17:24:16 +0800

    分支

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup, find_packages
 
 setup(name='graphite-to-zabbix',
-      version = '0.2',
+      version = '0.3',
       description = 'This tool allow handle alerts based on graphite metrics. It works as a proxy between graphite and zabbix. It use graphite as data source and zabbix as an alerting system.',
-      author = 'Alexey Dubkov',
-      author_email = 'alexey@dubkov.com',
-      url = 'https://github.com/blacked/graphite-to-zabbix',
+      author = 'tgavriltg',
+      author_email = 'tgavriltg@gmail.com',
+      url = 'https://github.com/tgavriltg/graphite-to-zabbix',
       install_requires = ["argparse","py-zabbix"],
       scripts = ['g2zproxy'],
       packages = find_packages(),
